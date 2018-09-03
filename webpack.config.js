ad10076e4a24e5b0e6667c3bf64dc608f8f2b004
Daniel Mies <danielmies@dmi-e.local>
2018-09-03 08:40:26 +0200

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,74 @@
 const path = require("path");
 const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 const pathsToClean = ["dist"];
 
 const cleanOptions = {
-    verbose: true,
-    dry: false
+  verbose: true,
+  dry: false
 };
 
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js"
-    },
-    resolve: {
-        extensions: [".js", ".vue"],
-        alias: {
-            vue: 'vue/dist/vue.min'
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(sass|scss)$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader!sass-loader"
-                })
-            },
-            {
-                test: /\.vue$/,
-                loader: "vue-loader",
-                options: {
-                    loaders: {
-                        scss: ExtractTextPlugin.extract({
-                            use: "css-loader!sass-loader",
-                            fallback: "vue-style-loader"
-                        }),
-                        sass: ExtractTextPlugin.extract({
-                            use: "css-loader!sass-loader?indentedSyntax",
-                            fallback: "vue-style-loader"
-                        })
-                    }
-                }
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js"
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+    alias: {
+      vue: "vue/dist/vue.min"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            query: {
+              importLoaders: 1
             }
+          },
+
+          "sass-loader"
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(pathsToClean, cleanOptions),
-        new ExtractTextPlugin("./styles.css?[hash]"),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "file-loader"
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+    new MiniCssExtractPlugin({ filename: "styles.css?[hash]" }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new VueLoaderPlugin()
+  ]
 };
